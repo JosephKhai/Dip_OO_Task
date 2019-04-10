b4b2838bf1a40db31f8a1bb09ee7108892f2d218
PUM SUAN KHAI <josephkhaipi@gmail.com>
2019-04-10 20:18:14 +1000

    First Week

---
+++ OO_Tasks/Shapes_Inheritence.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace OO_Tasks_Inheritence {
+    public abstract class Shape {
+        private string _name;
+        private string _colour;
+
+        protected Shape(string name, string colour)
+        {
+            _name = name;
+            _colour = colour;
+        }
+    }
+
+    public abstract class Quadrilateral: Shape{
+
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+        public double Side4 { get; set; }
+
+        public Quadrilateral(string _name, string _colour, double side1, double side2, double side3, double side4): base(_name, _colour)
+        {
+
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+            Side4 = side4;
+        }
+    }
+
+    public class Square:Quadrilateral{
+        public Square(string _colour, string _name, double side1) : base(_colour, _name, side1, side1, side1, side1)
+        {
+
+        }
+        public virtual double GetPerimeter()
+        {
+            double Perimeter = (4 * Side1);
+            return Perimeter;
+        }
+
+        public virtual double GetArea()
+        {
+            double AreaSquare = Math.Round((Side1 * Side1),2);
+            return AreaSquare;
+        }
+
+    }
+
+    public class Rectangle : Quadrilateral {
+
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+
+        public Rectangle(string _name, string _colour, double _side1, double _side2) : base( _name, _colour, _side1, _side2, _side1, _side2)
+        {
+            Side1 = _side1;
+            Side2 = _side2;
+        }
+
+        public virtual double GetPerimeter()
+        {
+
+            double Perimeter = 2*(Side1 + Side2);
+            return Perimeter;
+        }
+
+        public virtual double GetArea()
+        {
+            double GetArea =Math.Round((Side1 * Side2), 2);
+            return GetArea;
+        }
+    }
+
+    public class Circle:Shape {
+
+
+        //public double PI = Math.PI;
+        public double PI = 3.142;
+        public double Radius { get; set; }
+
+        public Circle(string _colour, string _name, double radius): base(_colour,_name)
+        {
+            Radius = radius;
+        }
+
+        public double GetPerimeter()
+        {
+            double Perimeter = Math.Round((2 * PI * Radius), 2);
+            return Perimeter;
+        }
+
+        public virtual double GetArea()
+        {
+
+            double GetArea = Math.Round((PI * (Radius * Radius)), 2);
+            return GetArea;
+        }
+
+        public virtual double GetCircumference()
+        {
+            return GetPerimeter();
+        }
+    }
+    
+    
+}
---
+++ OO_Tasks/Shapes_Interface.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace OO_Tasks_Interface {
+    public interface IShapeProps { }
+
+    public interface ICircleProps { }
+
+    public class Shape {
+        private string _name;
+        private string _colour;
+    }
+
+    public class Quadrilateral : Shape {
+        public double GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Square:Quadrilateral {
+      
+        public string _name { get; set; }
+        public string _colour { get; set; }
+        public double _side1 { get; set; }
+
+        public Square(string name, string colour, double side1)
+        {
+            _name = name;
+            _colour = colour;
+            _side1 = side1;
+        }
+
+        public virtual double GetPerimeter()
+        {
+            double Perimeter = (4 * _side1);
+            return Perimeter;
+        }
+
+        public virtual double GetArea()
+        {
+            double AreaSquare = Math.Round((_side1 * _side1), 2);
+            return AreaSquare;
+        }
+    }
+
+    public class Rectangle:Quadrilateral {
+
+       public string _name { get; set; }
+        public string _colour { get; set; }
+        public double _length { get; set; }
+        public double _width { get; set; }
+
+        public Rectangle(string name, string colour, double length, double width)
+        {
+            _name = name;
+            _colour = colour;
+            _length = length;
+            _width = width;
+        }
+
+        public virtual double GetPerimeter()
+        {
+
+            double Perimeter = 2 * (_length + _width);
+            return Perimeter;
+        }
+
+        public virtual double GetArea()
+        {
+            double GetArea = Math.Round((_length * _width), 2);
+            return GetArea;
+        }
+    }
+
+    public class Circle:Shape {
+
+        public string _name { get; set; }
+        public string _colour { get; set; }
+        public double _radius { get; set; }
+
+        public double PI = 3.142;
+        public Circle(string _name, string _colour, double _radius) 
+        {
+            this._radius = _radius;
+
+        }
+
+        public double GetPerimeter()
+        {
+            double Perimeter = Math.Round((2 * PI * _radius), 2);
+            return Perimeter;
+        }
+
+        public virtual double GetArea()
+        {
+
+            double GetArea = Math.Round((PI * (_radius * _radius)), 2);
+            return GetArea;
+        }
+
+        public virtual double GetCircumference()
+        {
+            return GetPerimeter();
+        }
+    }
+}
